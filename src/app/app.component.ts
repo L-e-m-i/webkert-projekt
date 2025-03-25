079be5b80e356e6a75859966a084809b1cc77fc3
Kerti Gábor Kelemen <h365410@stud.u-szeged.hu>
2025-03-25 20:57:55 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { tweetItem } from '../shared/models/tweetItem';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  items: tweetItem[] = [
+    new tweetItem(1, 'Tweet 1', 'User 1', '2025-01-01', 69, 420, 666),
+    new tweetItem(2, 'Tweet 2', 'User 2', '2025-01-02', 69, 420, 666)
+  ];
   title = 'Y';
 }
